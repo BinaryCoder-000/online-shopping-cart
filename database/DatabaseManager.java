9583798bdc2b688c6dd96dbc7d152c122084e154
Nitesh-up60 <niteshrajbhar221701@gmail.com>
2026-04-22 22:58:55 +0530

    Added Model and Database Folders

---
+++ database/DatabaseManager.java
@@ -0,0 +1,27 @@
+package com.shop.database;
+
+import java.io.*;
+import java.util.*;
+import com.shop.model.Product;
+
+public class DatabaseManager {
+
+    private static final String FILE = "products.dat";
+
+    @SuppressWarnings("unchecked")
+    public static List<Product> loadProducts() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE))) {
+            return (List<Product>) ois.readObject();
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static void saveProducts(List<Product> list) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE))) {
+            oos.writeObject(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
